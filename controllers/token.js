05cb5be8efcd05b0c150ea37b9a64c8349c4a311
Ed Tervit <edtervit@hotmail.co.uk>
2021-02-18 22:22:13 +0000

    added guest login route

--- controllers/token.js
+++ controllers/token.js
@@ -28,3 +28,40 @@ export const refreshToken = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const guest = async (req, res) => {
+  const refresh_token = process.env.SPOTIFY_REFRESH;
+
+  const pw = req.body.pw;
+
+  const thePW = process.env.REFRESH_PW;
+
+  try {
+    if (pw === thePW) {
+      const url = "https://accounts.spotify.com/api/token";
+      let authOptions = {
+        method: "POST",
+        body: `grant_type=refresh_token&refresh_token=${refresh_token}`,
+        headers: {
+          Authorization:
+            "Basic " +
+            new Buffer(
+              process.env.SPOTIFY_CLIENT_ID +
+                ":" +
+                process.env.SPOTIFY_CLIENT_SECRET
+            ).toString("base64"),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+
+      const response = await fetch(url, authOptions);
+      const json = await response.json();
+      res.status(201).json(json);
+    } else {
+      res.statusMessage = "Wrong Password";
+      res.status(401).end();
+    }
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
--- routes/TokenRoute.js
+++ routes/TokenRoute.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-import { refreshToken } from "../controllers/token.js";
+import { refreshToken, guest } from "../controllers/token.js";
 
 router.route("/refresh").post(refreshToken);
+
+router.route("/guest").post(guest);
 
 export default router;
